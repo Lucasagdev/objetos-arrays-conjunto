be127a3e3bbc57aa954bad471898cdfa65c7c738
Lucas Gomes <115755837+Lucasagdev@users.noreply.github.com>
2024-09-16 21:57:50 -0300

    Retirando comentários do código Javascript

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// let jogo = {
-//     nome: 'Final Fantasy'
-// }
+let jogo = {
+    nome: 'Final Fantasy'
+}
 
-// let jogo2 = {
-//     nome: 'Fallout'
-// }
+let jogo2 = {
+    nome: 'Fallout'
+}
 
-// let = videoGame = {
-//     nome: 'Xbox',
-//     valor: 3000,
-//     jogos: ['Final Fantasy', 'Fallout', 'Fifa']
-// }
+let = videoGame = {
+    nome: 'Xbox',
+    valor: 3000,
+    jogos: ['Final Fantasy', 'Fallout', 'Fifa']
+}
 
-// let jogo3 = {
-//     nome: 'Fifa'
-// }
+let jogo3 = {
+    nome: 'Fifa'
+}
 
-// videoGame.jogos.push(jogo3)
+videoGame.jogos.push(jogo3)
 
-// console.log(videoGame)
+console.log(videoGame)
 
 let cliente = {
     nome: 'Lucas',
